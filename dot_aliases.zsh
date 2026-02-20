5a1486f5491e80dd7c2e08d3f8fbbf97225ebe3d
Jacob O'Keeffe <jacob.okeeffe@proton.me>
2026-02-20 18:19:11 +0000

    Update .aliases.zsh

--- dot_aliases.zsh
+++ dot_aliases.zsh
@@ -10,3 +10,19 @@ alias cat='bat'
 
 # git
 alias gcmsg='git commit -m'
+alias gcm='git checkout master'
+
+# gh
+alias ghd='gh dash'
+alias ghpr='gh pr create'
+alias ghas='gh auth switch'
+alias ghcs='gh copilot suggest -s'
+
+# lazydocker/git
+alias lzd='lazydocker'
+alias lzg='lazygit'
+
+# cd
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
